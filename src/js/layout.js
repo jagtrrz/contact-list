128e5caaedd46778e50b6a7bcf5ac18ca68a6d8c
jagtrrz <jagutierrezc7@gmail.com>
2021-02-08 11:47:35 +0000

    start working

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,42 +1,27 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
 
-import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
+// import ScrollToTop from "./component/scrollToTop";
+
 import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
+import { Contact } from "./views/Contact.jsx";
+import { Addcontact } from "./views/Addcontact.jsx";
 
-//create your first component
-const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-	const basename = process.env.BASENAME || "";
-
+export const Layout = () => {
 	return (
-		<div className="d-flex flex-column">
-			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Navbar />
+		<div>
+			<BrowserRouter>
+				<div>
 					<Switch>
-						<Route exact path="/">
-							<Home />
-						</Route>
-						<Route exact path="/demo">
-							<Demo />
-						</Route>
-						<Route exact path="/single/:theid">
-							<Single />
-						</Route>
-						<Route>
-							<h1>Not found!</h1>
-						</Route>
+						<Route exact path="/index.html" component={Contact} />
+						<Route exact path="/" component={Contact} />
+						<Route exact path="/contacts" component={Contact} />
+						<Route exact path="/add" component={Addcontact} />
+						<Route exact path="/edit" component={Addcontact} />
+						<Route render={() => <h1 className="notfound">Not found!</h1>} />
 					</Switch>
-					<Footer />
-				</ScrollToTop>
+				</div>
 			</BrowserRouter>
 		</div>
 	);
